cc9a916d78b788eec725302dd3b8a5584ff163f8
Юля <varochko82@gmail.com>
2020-05-10 01:37:40 +0300

    ДЗ Блок 4 \5

--- src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
+++ src/ru/progwards/java1/lessons/compare_if_cycles/CyclesGoldenFibo.java
@@ -1,11 +1,11 @@
 package ru.progwards.java1.lessons.compare_if_cycles;
 
 public class CyclesGoldenFibo {
-    public static void main(String[] args) {
-
-    }
-    public static boolean containsDigit(int number, int digit){
-
-    }
+//    public static void main(String[] args) {
+//
+//    }
+//    public static boolean containsDigit(int number, int digit){
+//
+//    }
 
 }
