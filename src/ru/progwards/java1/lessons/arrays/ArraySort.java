92d31d672529090c168fc4ed1a85bfcd54b9e171
Юля <varochko82@gmail.com>
2020-06-02 23:50:53 +0300

    ДЗ Блок 6 \7

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -5,14 +5,15 @@ import java.util.Arrays;
 public class ArraySort {
 
     public static void main(String[] args) {
-    sort(new int [] {});
+    sort(new int [] {21,-25,38,5,7,0,-15,-3});
+        //System.out.println (new int[]{3,12,-5});
     }
 
     public static void sort(int[] a) {
-         a = new int[]{ 35, -21, -5, 6, 1, -18, 23, -3, 8, 15, -56};
-
+        // a = new int[]{ 35, -21, -5, 6, 1, -18, 23, -3, 8, 15, -56};
+          a = new int[]{};
         for (int num : a) {
-           // System.out.print(num + " ");
+            System.out.print(num + " ");
         }
         int count;
         do {
