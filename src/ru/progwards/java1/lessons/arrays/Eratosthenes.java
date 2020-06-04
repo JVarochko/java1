7d5773c8320fb7066eb2bcad06e98eefc0ead507
Юля <varochko82@gmail.com>
2020-06-04 23:37:51 +0300

    ДЗ Блок 6 \12

--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -1,19 +1,17 @@
 
 package ru.progwards.java1.lessons.arrays;
 
-import java.util.Arrays;
-
-import static java.util.Arrays.*;
-
 public class Eratosthenes {
+    int n;
     int sieve[];
     int N;
 
     public static void main(String args[]) {
         boolean sieve[];
-        //private void sift();
-    }
-    private boolean sieve(int n) {
+
+     }
+
+    private boolean sieve() {
 
         for (int i = 2; i <= N; i++) {
             boolean isPrime = true;
@@ -28,19 +26,16 @@ public class Eratosthenes {
                 }
             }
         }
-        return true;
+        return false;
     }
-    // конструктор который должен разместить массив sieve с
-    // размером в N и заполнить его значениями true, после чего вызвать метод sift()
+
     public Eratosthenes(int N) {
         this.sieve = sieve;
         this.N = N;
-        //fill(true);
+        //Arrays.fill [N] {true};
         sift();
     }
-    //который, собственно и реализует алгоритм Эратосфена, просеивая составные числа.
-    // Подсказка - нужно реализовать 2 вложенных цикла, внешний, например по i от 2 до N-1, и внутренний,
-    // например по j который будет просеивать числа, кратные переменной внешнего цикла i*j.
+
     private void sift() {
         for (int i = 2; i >= N - 1; i++) {
             //boolean isPrime = true;
@@ -54,26 +49,9 @@ public class Eratosthenes {
     }
 
     public boolean isSimple(int n) {
-        return sieve(n);
+        return sieve();
     }
-    // является ли число n простым, нужно взять значение по индексу
-    // в массиве. sieve[n]. Значение true, означает простое число, false - составное.
-    //private boolean[] sieve{
-    //  }
 }
-//который возвращает sieve[n], что бы можно было узнать,
-// простое число n или составное.
-
-//  public boolean isSimple (int n){
-
-// System.out.println(i);
-
-
-
-
-
-
-
 
 
 
