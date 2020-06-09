d6e1371fced029df38744f1afbb580fbaeceadd4
Юля <varochko82@gmail.com>
2020-06-10 00:05:41 +0300

    ДЗ Блок 6 \15

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -1,20 +1,53 @@
-//package ru.progwards.java1.lessons.arrays;
-//import java.util.Arrays;
-//public class DIntArray {
-//    public static void main(String[] args) {
-//
-//    }
-//
-//    private int []a;
-//    public DIntArray(){
-//
-//    }
-//    public void add(int num){
-//        num =1;
-//        int []a;
-//        int []a1=int []a + num;
-//
-//
-//
-//    }
-//}
+package ru.progwards.java1.lessons.arrays;
+
+import java.util.Arrays;
+
+public class DIntArray {
+    public static void main(String[] args) {
+
+
+    }
+
+    private int[] array = {};// private переменную - массив целых чисел.
+
+    public int[] getArray() { //доступ через getArray к private int[] array.
+        return array;
+    }
+
+    public DIntArray() {//конструктор по умолчанию
+
+    }
+
+    public void add(int num) {//добавляет элемент num в конец массива
+        int[] array = {8, 3, 9, 45, 12};
+        int[] array1 = new int[array.length + 1];
+        System.arraycopy(array, 0, array1, 0, num);
+        System.arraycopy(array, num, array1, 0, num + 1);
+        array = array1;
+    }
+
+    public void atInsert(int pos, int num) {//добавляет элемент num в позицию pos массива где-то посередине
+        int[] array = {8, 3, 9, 45, 12};
+        int[] array1 = new int[array.length + 1];
+        System.arraycopy(array, 0, array1, 0, num);
+        System.arraycopy(array, num, array1, 0, num + 1);
+        array = array1;
+    }
+
+    public void atDelete(int pos) {// удаляет элемент в позиции pos массива
+        int[] array1 = new int[array.length - 1];
+        System.arraycopy(array, 0, array1, 0, pos);
+        System.arraycopy(array, pos + 1, array1, pos, array.length - pos - 1);
+        array = array1;
+
+    }
+
+    public int at(int pos) {//возвращает элемент по индексу pos.
+        return pos;
+    }
+}
+
+
+
+
+
