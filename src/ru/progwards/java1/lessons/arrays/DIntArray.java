8e95ef7517b693003a2a122511340dca6ffa294e
Юля <varochko82@gmail.com>
2020-06-12 20:38:06 +0300

    ДЗ Блок 6 \17

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -8,54 +8,47 @@ public class DIntArray {
         DIntArray intArray = new DIntArray();
         for (int elem : array) {
             intArray.add(elem);
-            System.out.println(elem);
-
         }
-        Arrays.toString(intArray.getArray());
+        System.out.println(Arrays.toString(intArray.getArray()));
+        intArray.atInsert(3,2);
+        System.out.println(Arrays.toString(intArray.getArray()));
+        intArray.atDelete(3);
+        System.out.println(Arrays.toString(intArray.getArray()));
+        intArray.at(3);
         
-        System.out.println(intArray.getArray());
-
     }
 
     private int[] array = {};// private переменную - массив целых чисел.
-
     public int[] getArray() { //доступ через getArray к private int[] array.
         return array;
     }
-
     public DIntArray() {//конструктор по умолчанию
-
     }
-
     public void add(int num) {//добавляет элемент num в конец массива
-
         int[] array1 = new int[array.length + 1];
-        System.arraycopy(array, 0, array1, 0, array.length);
-        //System.arraycopy(array, num, array1, 0, num + 1);
+        int index =array.length;
+        System.arraycopy(array, 0, array1, 0, index);
         array1[array1.length - 1] = num;
-        System.out.println("add"+array.length);
         array = array1;
     }
-
     public void atInsert(int pos, int num) {//добавляет элемент num в позицию pos массива где-то посередине
-        int[] array = {8, 3, 9, 45, 12};
         int[] array1 = new int[array.length + 1];
-        System.arraycopy(array, 0, array1, 0, array.length);
-        System.arraycopy(array, num, array1, 0, num + 1);
-        array = array1;
-    }
-
-    public void atDelete(int pos) {// удаляет элемент в позиции pos массива
-        int[] array1 = new int[array.length - 1];
         System.arraycopy(array, 0, array1, 0, pos);
-        System.arraycopy(array, pos + 1, array1, pos, array.length - pos - 1);
+        System.arraycopy(array,pos, array1, pos+1, array.length-pos);
+        array1[pos] = num;
         array = array1;
-
     }
-
+    public void atDelete(int pos) {// удаляет элемент в позиции pos массива
+        int index = array.length;
+        int[] array1 = new int[array.length - 1];
+        System.arraycopy(array, 0, array1, 0, pos );
+        //System.arraycopy(array, pos - 1, array1, pos, array.length -pos-1);
+        System.arraycopy(array, pos+1, array1, pos, array.length -pos-1);
+        array = array1;
+    }
     public int at(int pos) {//возвращает элемент по индексу pos.
         return pos;
-    }
+    } //возвращает элемент по индексу pos.
 }
 
 
