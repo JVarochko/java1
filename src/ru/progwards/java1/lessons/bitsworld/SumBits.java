ef6c92c5177510dafc64a21e27dfd09beb182785
Юля <varochko82@gmail.com>
2020-06-22 21:16:52 +0300

    ДЗ Блок 7 \ 2

--- src/ru/progwards/java1/lessons/bitsworld/SumBits.java
+++ src/ru/progwards/java1/lessons/bitsworld/SumBits.java
@@ -3,52 +3,22 @@ package ru.progwards.java1.lessons.bitsworld;
 public class SumBits {
 
     public static void main(String[] args) {
-       sumBits((byte) 01001010);
-
+       sumBits((byte) 0100101);
+//       sumBits((byte) 100001);
+//       sumBits((byte) 10101010);
+//       sumBits((byte) 10101010);
     }
-
-//    public static int sumBits(byte value){
-//        int sum = 0;
-//        while (value > 0) sum+=value>>=1&1;
-//        System.out.println(sum);
-//        return sum;
-//    }
-
-//    public static int sumBits(byte value){
-//        int sum = 0;
-//        while (value>0){
-//            if (value%2 !=0) {
-//            } else {
-//                sum++;
-//            }
-//            value = (byte) (value >> 1);
-//
-//        }
-//        System.out.println(sum);
-//        return sum;
-//    }
-
-//    public static int sumBits(byte value){
-//        //int sum = value & 1;
-//        int sum = 0;
-//        while (value > 0) {
-//            sum += ((value >>= 1) & 1);
-//        }
-//        System.out.println(sum);
-//        return sum;
-//    }
     public static int sumBits(byte value){
         int sum = 0;
         while (value != 0) {
-            sum+=1; value >>=1&1;
+            if ((value & 1) != 0) sum++;
+            value >>= 1;
         }
         System.out.println(sum);
         return sum;
     }
 
-    }
-
-
+  }
 
 
 
