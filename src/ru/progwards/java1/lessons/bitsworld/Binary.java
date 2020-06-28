5557207d2c70b16904295f08ffe16aaf6b3718a8
Юля <varochko82@gmail.com>
2020-06-28 14:39:47 +0300

    ДЗ Блок 7 \ 11

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -7,12 +7,13 @@ public class Binary {
     }
 
     public String toString() {
-
         //byte num=0;
         //String bin = String.format(Integer.toBinaryString(num));
-        String bin = String.format("%8s", Integer.toBinaryString(num)).replace(' ', ' ');
+        //String bin = String.format("%8s", Integer.toBinaryString(num)).replace(' ', ' ');
+        String bin = String.format("%8s", Integer.toBinaryString(num));
         System.out.println(bin);
         return bin;
+
     }
 
     public static void main(String[] args) {
