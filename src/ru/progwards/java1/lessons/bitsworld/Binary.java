aca9e0eb179953ecaaac4298812fc29b30cecc76
Юля <varochko82@gmail.com>
2020-06-28 14:58:38 +0300

    ДЗ Блок 7 \ 13

--- src/ru/progwards/java1/lessons/bitsworld/Binary.java
+++ src/ru/progwards/java1/lessons/bitsworld/Binary.java
@@ -1,5 +1,7 @@
 package ru.progwards.java1.lessons.bitsworld;
 
+import java.math.BigInteger;
+
 public class Binary {
     byte num;
 
@@ -7,13 +9,17 @@ public class Binary {
     }
 
     public String toString() {
+
         //byte num=0;
         //String bin = String.format(Integer.toBinaryString(num));
         //String bin = String.format("%8s", Integer.toBinaryString(num)).replace(' ', ' ');
-        String bin = String.format("%8s", Integer.toBinaryString(num));
-        System.out.println(bin);
+//        String bin = String.format("%8s", Integer.toBinaryString(num));
+//        System.out.println(bin);
+//        return bin;
+        String s = ""+num;
+        BigInteger bi = new BigInteger(s);
+        String bin = bi.toString(2);
         return bin;
-
     }
 
     public static void main(String[] args) {
