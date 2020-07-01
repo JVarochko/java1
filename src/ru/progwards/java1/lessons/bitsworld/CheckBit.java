8bc17f795b8511918dc1b91f188e48a2b647a41c
Юля <varochko82@gmail.com>
2020-07-02 00:24:06 +0300

    ДЗ Блок 7 \ 19

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -2,22 +2,19 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class CheckBit {
     public static void main(String[] args) {
-checkBit((byte) 8,8);
-        System.out.println(checkBit((byte)8,8));
+  checkBit((byte) 1,1);
+        //System.out.println(checkBit((byte)1,1));
 
     }
 
     public static int checkBit(byte value, int bitNumber){
-
-
-    byte aByte = (byte)0b00100001;
-        //String str = 0;
-//        bitNumber = 0;
-//        for (int i = 0; i < str.length(); i++) {
-//            bitNumber <<= 1;
-//            if (str.charAt(i) == '1') bitNumber++;
-//        }
-        return bitNumber;
+        int val = value & 0b1111_1111;
+     byte [] array = new byte [8];
+        for (int i = 0; i < 8; i++) {
+            System.out.println(val+bitNumber);
+        }
+        val = val >> 1;
+     return bitNumber;
     }
 
 }
