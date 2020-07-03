8f301ddbc13376122ef3c0c696963126fae9287a
Юля <varochko82@gmail.com>
2020-07-04 00:30:00 +0300

    ДЗ Блок 7 \ 24

--- src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
+++ src/ru/progwards/java1/lessons/bitsworld/CheckBit.java
@@ -2,13 +2,15 @@ package ru.progwards.java1.lessons.bitsworld;
 
 public class CheckBit {
     public static void main(String[] args) {
-  checkBit((byte)0,0);
+  checkBit((byte)0b00100001,2);
+        System.out.println(checkBit((byte)0b11111111,2));
+        System.out.println(checkBit((byte)0b00100001,0));
 
        }
     public static int checkBit(byte value, int bitNumber){
-      int val = value & 0b1111_1111;
-      val = val >> bitNumber;
-      return val%2;
+      int val = value & 0b1111_1111;// приведение к int
+      val = val >> bitNumber; // обитовый сдвиг вправо на bitNumber
+      return val%2;// возвращаем результат остатка от деления на 2
     }
 
 }
