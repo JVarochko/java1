9a4777e3abdb3baa42a051424e5fe8df1859274d
Юля <varochko82@gmail.com>
2020-07-22 23:46:15 +0300

    ДЗ Блок  8\16

--- src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
+++ src/ru/progwards/java1/lessons/interfaces/CalculateFibonacci.java
@@ -2,58 +2,56 @@ package ru.progwards.java1.lessons.interfaces;
 
 public class CalculateFibonacci {
 
+    private static CacheInfo lastFibo;
+    //public static  CacheInfo getlastFibo;
 
-    private static  CacheInfo lastFibo;
-    public static  CacheInfo getlastFibo;
-
-    public static Object fiboNumber(int n) {
+    public static int fiboNumber(int n) {
+        //if (n==lastFibo.n){
+           // return n;
         int a = 1;
         int b = 1;
         int c = 0;
-        //int lastFibo = CacheInfo lastFibo;
 
-
-            if (n == 1 | n == 2) {
-                c = 1;
-            } else {
-                for (int i = 0; i <= n - 3; i++) {
-                    c = a + b;
-                    a = b;
-                    b = c;
-                }
+        if (n == 1 | n == 2) {
+            c = 1;
+        } else {
+            for (int i = 0; i <= n - 3; i++) {
+                c = a + b;
+                a = b;
+                b = c;
             }
-            System.out.println(c);
-            return c;
+        }
+        //lastFibo = new CacheInfo(n,c);
+        lastFibo = new CacheInfo(n,c);
 
-//        if (n == c) {
-//           return int;
-//        }
-//        else {
-//            return c; //  c == lastFibo если с!=n то с равно lastFibo
-//
-//            }
+        return c;
+
+
+    }
+
+    public static class CacheInfo {  //вложенный класс
+
+        public int n; //число, для которого рассчитываем Фибоначчи
+        public int fibo; //результат расчета
+
+        public CacheInfo(int n, int fibo) {
+            this.n = n;
+            this.fibo = fibo;
         }
 
-        public static CacheInfo getLastFibo () {
+        public static CacheInfo getLastFibo() {
 
             return lastFibo;
         }
-        public static void clearLastFibo () {
-            //(int)lastFibo = 0;
+
+        public static void clearLastFibo() {
+            lastFibo = null;
         }
+    }
 
-        public static class CacheInfo {  //вложенный класс
-
-            public int n; //число, для которого рассчитываем Фибоначчи
-            public int fibo; //результат расчета
-
-
-        }
-
-        public static void main (String[]args){
-            fiboNumber(2);
-        }
-
+    public static void main(String[] args) {
+        fiboNumber(2);
+    }
 
 }
 
